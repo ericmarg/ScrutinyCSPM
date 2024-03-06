8ba5472be456fbcd1cc2a2ee3b747570be129fcc
Daniel Healy <daniel.addison.healy@gmail.com>
2024-03-05 21:28:09 -0500

    Cleaned up aws.py formatting

--- src/scrutinycspm/providers/aws/aws.py
+++ src/scrutinycspm/providers/aws/aws.py
@@ -14,9 +14,7 @@ for bucket in buckets_list['Buckets']:
     bucket_name = bucket['Name']
 
     try:
-        response = s3.get_public_access_block(
-            Bucket=bucket_name
-        )
+        response = s3.get_public_access_block(Bucket=bucket_name)
         public_access_block_dict[bucket_name] = response['PublicAccessBlockConfiguration']
     except botocore.exceptions.ClientError: # handles case where 'Block All Public Access' is OFF
         public_access_block_dict[bucket_name] = '{}'
