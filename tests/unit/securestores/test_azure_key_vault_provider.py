3c763d776dd7c64995fabe79ed3485c016117838
Robert <robert@oakleyrain.com>
2024-03-11 23:36:03 -0500

    Tested Authenticate with certificate

--- tests/unit/securestores/test_azure_key_vault_provider.py
+++ tests/unit/securestores/test_azure_key_vault_provider.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from hydra.core.global_hydra import GlobalHydra
 import hydra
 from ScrutinyCSPM.src.scrutinycspm.access.securestores.azure_keyvault_provider import AzureKeyVaultProvider
-
+from datetime import datetime
 
 class TestHashicorpVaultProvider(TestCase):
 
@@ -28,5 +28,16 @@ class TestHashicorpVaultProvider(TestCase):
         print(akv_provider.read_secret("hashicorp-vault-password02"))
         assert akv_provider.read_secret("hashicorp-vault-password02") == {'value': 'password12345'}
 
+    def test_write_secret(self):
+        akv_provider = AzureKeyVaultProvider(self.cfg_secure.vault.key_vault_url)
+        akv_provider.authenticate_with_certificate(self.cfg_secure.vault.tenant_id, self.cfg_secure.vault.client_id, certificate_path=self.cfg_secure.vault.certificate_path)
+        # Get the current date
+        current_date = datetime.now()
+
+        # Format the date as a string
+        date_string = current_date.strftime("%Y-%m-%d")
+        print(akv_provider.read_secret("updates-test01"))
+        akv_provider.write_secret("updates-test01", date_string)
+        assert akv_provider.read_secret("updates-test01") == {'value': f'{date_string}'}
     def tearDown(self):
         GlobalHydra.instance().clear()
