60170dd1fbd688d8e83717a31ece58c8be39cac7
Daniel Healy <daniel.addison.healy@gmail.com>
2024-03-12 20:07:46 -0400

    Added logic to handle mapping to AllPublicAccessBlocked property

--- src/scrutinycspm/providers/aws/aws.py
+++ src/scrutinycspm/providers/aws/aws.py
@@ -8,18 +8,27 @@ s3 = boto3.client('s3')
 
 buckets_list = s3.list_buckets() # gathers all S3 buckets in target account
 public_access_block_dict = {}
-bucket_versioning_dict = {}
+bucket_scan_dict = {}
 
 # loops through all buckets, obtaining the PublicAccessBlock Configuration
 for bucket in buckets_list['Buckets']:
     bucket_name = bucket['Name']
 
     bucket_properties = {}
+    bucket_properties['AllPublicAccessBlocked'] = True
+
     try:
         response = s3.get_public_access_block(Bucket=bucket_name)
+        for config_item in response['PublicAccessBlockConfiguration']:
+            if response['PublicAccessBlockConfiguration'][config_item] is True:
+                continue
+            else:
+                bucket_properties['AllPublicAccessBlocked'] = False
+                break
+            
         public_access_block_dict[bucket_name] = response['PublicAccessBlockConfiguration']
     except botocore.exceptions.ClientError: # handles case where 'Block All Public Access' is OFF
-        public_access_block_dict[bucket_name] = '{}'
+        bucket_properties['AllPublicAccessBlocked'] = False
 
     try:
         bucket_versioning_status = s3.get_bucket_versioning(Bucket=bucket_name)['Status']
@@ -27,15 +36,13 @@ for bucket in buckets_list['Buckets']:
             bucket_properties['VersioningEnabled'] = True
         else:
             bucket_properties['VersioningEnabled'] = False
-        bucket_versioning_dict[bucket_name] = bucket_properties
+        bucket_scan_dict[bucket_name] = bucket_properties
     except KeyError: # handles case where 'Bucket versioning' has never been turned on
         bucket_properties['VersioningEnabled'] = False
-        bucket_versioning_dict[bucket_name] = bucket_properties
+        bucket_scan_dict[bucket_name] = bucket_properties
 
-print('Public Access Block report')
-print(json.dumps(public_access_block_dict))
 print()
-print('Bucket Versioning report')
-print(json.dumps(bucket_versioning_dict))
+print('Bucket Scan report')
+print(json.dumps(bucket_scan_dict))
 print()
 print('End of S3 Report\n')
