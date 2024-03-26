236d8b98925f8e79ab858eba95214ae5181deccd
Robert <robert@oakleyrain.com>
2024-03-26 18:21:24 -0500

    Minor Changes

--- cli/commands/command_manager.py
+++ cli/commands/command_manager.py
@@ -16,16 +16,50 @@ class CommandPlugin(Protocol):
 
 
 class CommandManager:
+    """
+    A class that manages commands for the CLI application.
+    """
+
     def __init__(self):
         self.commands = {}
 
     def register_command(self, name: str, command_class: Callable[..., CommandPlugin]):
+        """
+        Registers a command with the given name and command class.
+
+        Args:
+            name (str): The name of the command.
+            command_class (Callable[..., CommandPlugin]): The command class.
+
+        Returns:
+            None
+        """
         self.commands[name] = command_class
 
     def unregister_command(self, name: str):
+        """
+        Unregisters a command with the given name.
+
+        Args:
+            name (str): The name of the command.
+
+        Returns:
+            None
+        """
         self.commands.pop(name, None)
 
     def execute_command(self, name: str, *args, **kwargs):
+        """
+        Executes a command with the given name and arguments.
+
+        Args:
+            name (str): The name of the command.
+            *args: Variable length arguments.
+            **kwargs: Keyword arguments.
+
+        Returns:
+            The result of the command execution.
+        """
         if name not in self.commands:
             raise ValueError(f"Command '{name}' not found.")
 
@@ -34,7 +68,6 @@ class CommandManager:
         if "--help" in args:
             command = command_class()
             return command.help()
-
 
         command = command_class(*args, **kwargs)
         return command.execute()
