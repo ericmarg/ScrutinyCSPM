50c55dcaaa51f2ad498e8dba266b734fb55bdaf4
Robert <robert@oakleyrain.com>
2024-03-26 16:46:41 -0500

    Added Looping to CLI

--- cli/main.py
+++ cli/main.py
@@ -1,26 +1,18 @@
-import argparse
-import json
 from dataclasses import dataclass
 import hydra
 from omegaconf import DictConfig
 from typing import Any, Dict
 from commands.command_manager import CommandManager
-
 import logging
 import importlib
 
-@hydra.main(config_path="config", config_name="conf")
+
+@hydra.main(config_path="config", config_name="conf", version_base=None)
 def main(cfg: DictConfig) -> None:
+    logging.basicConfig(level=cfg.logging.level, filename="app.log")
     
-    logging.basicConfig(level=cfg.logging.level, filename='app.log')
-
-    parser = argparse.ArgumentParser(description="ScrutinyCSPM CLI")
-    parser.add_argument("command", nargs="?", help="The command to execute")
-    parser.add_argument("args", nargs="*", help="Additional arguments for the command")
-
-    # Parse the command-line arguments
-    args = parser.parse_args()
-
+    print("Welcome to Scrutiny CSP CLI")
+    
     command_manager = CommandManager()
 
     # Load command plugins dynamically based on the Hydra configuration
@@ -29,23 +21,26 @@ def main(cfg: DictConfig) -> None:
         command_class = getattr(module, command_config.class_name)
         command_manager.register_command(command_name, command_class)
 
-    # Check if a command is provided
-    if args.command:
-        command_name = args.command
-        command_args = args.args
+    while True:
+        command = input("Enter a command (or 'quit' to exit): ")
+
+        if command.lower() == "quit":
+            print("Exiting...")
+            break
+
+        command_parts = command.split()
+        command_name = command_parts[0]
+        command_args = command_parts[1:]
 
         try:
-            return command_manager.execute_command(command_name, *command_args)
+            result = command_manager.execute_command(command_name, *command_args)
+            if result is not None:
+                print(result)
         except ValueError as e:
             print(f"Error: {str(e)}")
             print("Available commands:")
             for cmd in command_manager.commands:
                 print(f"- {cmd}")
-    else:
-        # No command provided, display available commands
-        print("Available commands:")
-        for cmd in command_manager.commands:
-            print(f"- {cmd}")
 
 
 if __name__ == "__main__":
