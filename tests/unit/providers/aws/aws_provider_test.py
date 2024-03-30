d82bd25507fdca950247133bf55755b19c96c173
Greg Frasco <frascog17@gmail.com>
2024-03-30 18:18:27 -0400

    feat: update aws fixutres

--- tests/unit/providers/aws/aws_provider_test.py
+++ tests/unit/providers/aws/aws_provider_test.py
@@ -10,13 +10,19 @@ patcher.start()
 
 with open('./aws.json') as f:
     AWS_DATA = json.load(f)
+
+boto3_mock.client.return_value.get_caller_identity.return_value = AWS_DATA['get_caller_identity']
+boto3_mock.client.return_value.get_paginator.return_value.paginate.return_value = dict(AWS_DATA['describe_instances'])
+
+account = AWSAccount()
+
 class TestAWSAccount(unittest.TestCase):
     def test_aws_account(self):
-        boto3_mock.client.return_value.get_caller_identity.return_value = AWS_DATA['get_caller_identity']
-
-        account = AWSAccount()
-
         self.assertEqual(account.id, AWS_DATA['get_caller_identity']['Account'])
+
+    def test_get_vms(self):
+        self.assertEqual(len(account.vms), 1)
+        self.assertEqual(account.vms[0].id, AWS_DATA['describe_instances']['Reservations'][0]['Instances'][0]['InstanceId'])
 
 if __name__ == '__main__':
     unittest.main()
