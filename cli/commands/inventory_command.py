76fb5b8946daded7b1495794d983bbe11f5bd659
Robert <robert@oakleyrain.com>
2024-04-01 09:55:46 -0500

    merging changes

--- cli/commands/inventory_command.py
+++ cli/commands/inventory_command.py
@@ -8,6 +8,7 @@ from opa_client.errors import ConnectionsError
 import logging
 from src.scrutinycspm.access.repository.github_provider import GitHubRepository
 import os
+from hydra.core.global_hydra import GlobalHydra
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -15,37 +16,68 @@ logging.basicConfig(
 
 
 class InventoryCommand(CommandPlugin):
+    """
+    A command plugin for executing inventory related operations.
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Initialize the InventoryCommand object.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         logging.debug("Initialized InventoryCommand")
         self.args = args
         self.kwargs = kwargs
+        self.github = None
+        GlobalHydra.instance().clear() 
         with initialize(version_base=None, config_path="../config", job_name="inventory_command"):
             self.cfg = compose(config_name="conf")
 
-        
-
     @add_logging
     def execute(self):
+        """
+        Execute the inventory command.
+
+        Returns:
+            The result of the inventory command.
+        """
         self.log_info(f"Executing {self.__class__.__name__}")
         logging.debug("Calling InventoryCommand.execute")
 
         return self.get_inventory()
 
     def help(self):
-        return "Usage: inventory returns the result."
-    
+        """
+        Get the help message for the inventory command.
 
+        Returns:
+            The help message.
+        """
+        return "Usage: inventory returns the security settings from cloud providers."
 
-    def get_policies(self): 
-        
+    def get_policies(self):
+        """
+        Get the policies from the GitHub repository.
+
+        Returns:
+            The policies from the GitHub repository.
+        """
         if self.cfg is not None:
-            github = GitHubRepository(self.cfg.policies.policy_repo)
-            policies = github.get_files_by_extension(self.cfg.policies.path, ".rego")
+            self.github = GitHubRepository(self.cfg.policy.github.repository)
+            policies = self.github.get_files_by_extension(self.cfg.policy.github.path, ".rego")
             return policies
-    
-
 
     def get_inventory(self):
+        """
+        Get the inventory.
+
+        Returns:
+            The inventory.
+        """
+        policies = self.get_policies()
 
         aws = AWSAccount()
         opa = OpaClient()
@@ -55,7 +87,6 @@ class InventoryCommand(CommandPlugin):
 
             container_dict = container.to_dict()
             try:
-                
                 opa.update_opa_policy_fromfile(
                     filepath="policies/object_storage.rego", endpoint="obj_storage"
                 )
@@ -75,6 +106,12 @@ class InventoryCommand(CommandPlugin):
         return aws
 
     def format_output(self, container):
+        """
+        Format the output for a container.
+
+        Args:
+            container: The container to format the output for.
+        """
         print(
-            f"Name: {container.name}, PublicAccessBlocked: {container.all_public_access_blocked}, Region: {container.region}, VersioningEnabled: {container.versioning_enabled}"
+            f"Name: {container.name}, PublicAccessBlocked: {container.all_public_access_blocked}, VersioningEnabled: {container.versioning_enabled}"
         )
--- src/scrutinycspm/access/repository/github_provider.py
+++ src/scrutinycspm/access/repository/github_provider.py
@@ -2,7 +2,16 @@ from typing import List, Optional
 from github import Github, Repository as GitHubRepo, GithubException
 from github.AuthenticatedUser import AuthenticatedUser
 
+
 class GitHubRepository:
+    """
+    Represents a GitHub repository.
+
+    Args:
+        repo_name (str): The name of the repository.
+        access_token (str, optional): The access token for authentication. Defaults to None.
+    """
+
     def __init__(self, repo_name: str, access_token: Optional[str] = None):
         if access_token:
             self.github = Github(access_token)
@@ -12,42 +21,118 @@ class GitHubRepository:
 
     @property
     def name(self) -> str:
+        """
+        Get the name of the repository.
+
+        Returns:
+            str: The name of the repository.
+        """
         return self.repo.name
 
     @property
     def description(self) -> str:
+        """
+        Get the description of the repository.
+
+        Returns:
+            str: The description of the repository.
+        """
         return self.repo.description
 
     @property
     def url(self) -> str:
+        """
+        Get the URL of the repository.
+
+        Returns:
+            str: The URL of the repository.
+        """
         return self.repo.html_url
 
     @property
     def is_private(self) -> bool:
+        """
+        Check if the repository is private.
+
+        Returns:
+            bool: True if the repository is private, False otherwise.
+        """
         return self.repo.private
 
     def get_branches(self) -> List[str]:
-        return [branch.name for branch in self.repo.get_branches()]
-    
-    def get_branch(self, branch_name) -> str:    
-        self.repo.get_branch(branch_name)
+        """
+        Get a list of branch names in the repository.
 
+        Returns:
+            List[str]: A list of branch names.
+        """
+        return [branch.name for branch in self.repo.get_branches()]
+
+    def get_branch(self, branch_name) -> str:
+        """
+        Get a specific branch in the repository.
+
+        Args:
+            branch_name (str): The name of the branch.
+
+        Returns:
+            str: The branch object.
+        """
+        return self.repo.get_branch(branch_name)
 
     def get_commits(self, branch: str) -> List[str]:
+        """
+        Get a list of commit SHAs in a specific branch.
+
+        Args:
+            branch (str): The name of the branch.
+
+        Returns:
+            List[str]: A list of commit SHAs.
+        """
         commits = self.repo.get_commits(sha=branch)
         return [commit.sha for commit in commits]
 
     def get_file_contents(self, file_path: str, branch: str) -> str:
+        """
+        Get the contents of a file in a specific branch.
+
+        Args:
+            file_path (str): The path of the file.
+            branch (str): The name of the branch.
+
+        Returns:
+            str: The contents of the file.
+        
+        Raises:
+            FileNotFoundError: If the file is not found in the branch.
+            GithubException: If an error occurs while accessing the GitHub API.
+        """
         try:
             contents = self.repo.get_contents(file_path, ref=branch)
             return contents.decoded_content.decode("utf-8")
         except GithubException as e:
             if e.status == 404:
-                raise FileNotFoundError(f"File '{file_path}' not found in branch '{branch}'.")
+                raise FileNotFoundError(
+                    f"File '{file_path}' not found in branch '{branch}'."
+                )
             else:
                 raise e
 
     def get_files_by_extension(self, folder_path: str, file_extension: str) -> list:
+        """
+        Get a list of file paths with a specific file extension in a folder.
+
+        Args:
+            folder_path (str): The path of the folder.
+            file_extension (str): The file extension.
+
+        Returns:
+            list: A list of file paths.
+
+        Raises:
+            GithubException: If an error occurs while accessing the GitHub API.
+        """
         try:
             contents = self.repo.get_contents(folder_path)
             filtered_files = []
@@ -56,7 +141,9 @@ class GitHubRepository:
                 file_content = contents.pop(0)
                 if file_content.type == "dir":
                     contents.extend(self.repo.get_contents(file_content.path))
-                elif file_content.type == "file" and file_content.path.endswith(file_extension):
+                elif file_content.type == "file" and file_content.path.endswith(
+                    file_extension
+                ):
                     filtered_files.append(file_content.path)
 
             return filtered_files
@@ -64,8 +151,22 @@ class GitHubRepository:
         except GithubException as e:
             print(f"Error: {e}")
             return []
-        
+
     def create_branch(self, branch_name: str, source_branch) -> None:
+        """
+        Create a new branch in the repository.
+
+        Args:
+            branch_name (str): The name of the new branch.
+            source_branch: The source branch to create the new branch from.
+
+        Raises:
+            PermissionError: If the access token is required to create a branch.
+            GithubException: If an error occurs while accessing the GitHub API.
+
+        Returns:
+            None
+        """
         try:
             user: AuthenticatedUser = self.github.get_user()
         except GithubException as e:
@@ -73,37 +174,106 @@ class GitHubRepository:
                 raise PermissionError("Access token is required to create a branch.")
             else:
                 raise e
-        
+
         source_branch = self.repo.get_branch(self.repo.default_branch)
-        self.repo.create_git_ref(ref=f"refs/heads/{branch_name}", sha=source_branch.commit.sha)
+        self.repo.create_git_ref(
+            ref=f"refs/heads/{branch_name}", sha=source_branch.commit.sha
+        )
 
     def delete_branch(self, branch_name: str) -> None:
+        """
+        Deletes a branch from the GitHub repository.
+
+        Args:
+            branch_name (str): The name of the branch to be deleted.
+
+        Raises:
+            PermissionError: If the access token is required to delete a branch.
+            GithubException: If an error occurs while accessing the GitHub API.
+
+        Returns:
+            None
+        """
         try:
             user: AuthenticatedUser = self.github.get_user()
         except GithubException as e:
             if e.status == 401:
-                raise PermissionError("Access token is required to delete a branch.")
+                raise PermissionError(
+                    "Access token is required to delete a branch."
+                )
             else:
                 raise e
-        
+
+        # Get the branch object
         branch = self.repo.get_branch(branch_name)
+
+        # Delete the branch using the GitHub API
         self.repo._requester.requestJsonAndCheck(
-            "DELETE",
-            self.repo.url + f"/git/refs/heads/{branch_name}"
+            "DELETE", self.repo.url + f"/git/refs/heads/{branch_name}"
         )
 
-    def create_file(self, file_path: str, content: str, branch: str, commit_message: str) -> None:
+    def create_file(
+        self, file_path: str, content: str, branch: str, commit_message: str
+    ) -> None:
+        """
+        Create a new file in the repository.
+
+        Args:
+            file_path (str): The path of the new file.
+            content (str): The content of the new file.
+            branch (str): The name of the branch to create the file in.
+            commit_message (str): The commit message for the file creation.
+
+        Raises:
+            PermissionError: If the access token is required to create a file.
+
+        Returns:
+            None
+        """
         if not self.github.get_user().get_repo(self.repo.full_name):
             raise PermissionError("Access token is required to create a file.")
         self.repo.create_file(file_path, commit_message, content, branch=branch)
 
-    def update_file(self, file_path: str, content: str, branch: str, commit_message: str) -> None:
+    def update_file(
+        self, file_path: str, content: str, branch: str, commit_message: str
+    ) -> None:
+        """
+        Update an existing file in the repository.
+
+        Args:
+            file_path (str): The path of the file to update.
+            content (str): The new content of the file.
+            branch (str): The name of the branch to update the file in.
+            commit_message (str): The commit message for the file update.
+
+        Raises:
+            PermissionError: If the access token is required to update a file.
+
+        Returns:
+            None
+        """
         if not self.github.get_user().get_repo(self.repo.full_name):
             raise PermissionError("Access token is required to update a file.")
         contents = self.repo.get_contents(file_path, ref=branch)
-        self.repo.update_file(file_path, commit_message, content, contents.sha, branch=branch)
+        self.repo.update_file(
+            file_path, commit_message, content, contents.sha, branch=branch
+        )
 
     def delete_file(self, file_path: str, branch: str, commit_message: str) -> None:
+        """
+        Delete an existing file from the repository.
+
+        Args:
+            file_path (str): The path of the file to delete.
+            branch (str): The name of the branch to delete the file from.
+            commit_message (str): The commit message for the file deletion.
+
+        Raises:
+            PermissionError: If the access token is required to delete a file.
+
+        Returns:
+            None
+        """
         if not self.github.get_user().get_repo(self.repo.full_name):
             raise PermissionError("Access token is required to delete a file.")
         contents = self.repo.get_contents(file_path, ref=branch)
