2f4bd49855b50ac7fd278730661a8088a51d9e6f
Greg Frasco <frascog17@gmail.com>
2024-04-11 22:50:06 -0400

    feat: update result page icons

--- frontend/src/app/dashboard/[scan]/results/page.tsx
+++ frontend/src/app/dashboard/[scan]/results/page.tsx
@@ -5,9 +5,10 @@ import React from 'react';
 import { InfoCard } from '@/components/info-card';
 import { ResourceList } from '@/components/resource-list';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAws, faBitbucket } from '@fortawesome/free-brands-svg-icons';
+import { faAws, faBitbucket, faWindows } from '@fortawesome/free-brands-svg-icons';
 import { ResourceListItem } from '@/components/resource-list-item';
 import { ProgressBar } from '@/components/progress-bar';
+import { faServer, faUserShield } from '@fortawesome/free-solid-svg-icons';
 
 export default function Dashboard() {
   return (
@@ -37,21 +38,21 @@ export default function Dashboard() {
           <Grid item xs={12} sm={6} md={4} lg={4}>
             <ResourceList name="Object Storage Buckets" icon={<FontAwesomeIcon icon={faBitbucket} />} openIssues={5} total={24}>
               <ResourceListItem name="aws-storage-bucket" icon={<FontAwesomeIcon icon={faAws} />} />
-              <ResourceListItem name="azure-storage-bucket" icon={<FontAwesomeIcon icon={faAws} />} />
+              <ResourceListItem name="azure-storage-bucket" icon={<FontAwesomeIcon icon={faWindows} />} />
               <ResourceListItem name="aws-storage-bucket" icon={<FontAwesomeIcon icon={faAws} />} />
             </ResourceList>
           </Grid>
           <Grid item xs={12} sm={6} md={4} lg={4}>
-            <ResourceList name="Virtual Machines" icon={<FontAwesomeIcon icon={faBitbucket} />} openIssues={6} total={15}>
+            <ResourceList name="Virtual Machines" icon={<FontAwesomeIcon icon={faServer} />} openIssues={6} total={15}>
               <ResourceListItem name="aws-vm-1" icon={<FontAwesomeIcon icon={faAws} />} />
-              <ResourceListItem name="azure-vm-2" icon={<FontAwesomeIcon icon={faAws} />} />
+              <ResourceListItem name="azure-vm-2" icon={<FontAwesomeIcon icon={faWindows} />} />
               <ResourceListItem name="aws-vm-3" icon={<FontAwesomeIcon icon={faAws} />} />
             </ResourceList>
           </Grid>
           <Grid item xs={12} sm={6} md={4} lg={4}>
-            <ResourceList name="Roles & Permissions" icon={<FontAwesomeIcon icon={faBitbucket} />} openIssues={6} total={24}>
+            <ResourceList name="Roles & Permissions" icon={<FontAwesomeIcon icon={faUserShield} />} openIssues={6} total={24}>
               <ResourceListItem name="aws-iam-role" icon={<FontAwesomeIcon icon={faAws} />} />
-              <ResourceListItem name="azure-iam-role" icon={<FontAwesomeIcon icon={faAws} />} />
+              <ResourceListItem name="azure-iam-role" icon={<FontAwesomeIcon icon={faWindows} />} />
               <ResourceListItem name="aws-iam-role-2" icon={<FontAwesomeIcon icon={faAws} />} />
             </ResourceList>
           </Grid>
