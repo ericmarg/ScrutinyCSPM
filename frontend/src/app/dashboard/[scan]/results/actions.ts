42b7ee4361cccf03a6c289201aa6828faec06c69
Greg Frasco <frascog17@gmail.com>
2024-04-15 17:02:18 -0700

    feat: create main docker file

--- frontend/src/app/dashboard/[scan]/results/actions.ts
+++ frontend/src/app/dashboard/[scan]/results/actions.ts
@@ -1,24 +1,13 @@
 'use server';
-
-import { statSync, readdirSync, existsSync, mkdirSync } from 'node:fs';
-import path from 'node:path';
-import { ScanListItem } from '@/types/scan-list-item';
+import { exec } from "node:child_process";
 import { Scan } from '@/types/scan';
-import { readFileSync } from 'fs';
 
 export async function getScan(id: string) {
-  let scans: Scan;
+  const scan: Scan;
   try {
-    if (existsSync('scans/keys')) {
-      mkdirSync('scans/leys', { recursive: true });
-    }
-    const keyFile = readFileSync('scans/keys/' + id + '.pem', 'utf8');
-    const scanFiles = readdirSync('scans').filter((file) => file.endsWith('.json'));
-    const files = scanFiles[Number(id)];
-    const filePath = path.join('scans', files);
-    const scanFile = readFileSync(filePath, 'utf8');
-
-    return 'test';
+    const pythonProcess = exec(`python ../cli/main.py `);
+    pythonProcess.kill();
+    return null;
   } catch (error) {
     return {};
   }
